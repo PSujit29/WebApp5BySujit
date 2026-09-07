7ff43a1106898f39929c97762a0ff364b5af2ac6
PSujit29 <iamsujeet29@gmail.com>
2026-09-07 20:07:42 +0545

    Add WebApp5BySujit with Dockerfile for Lab 23 deployment

---
+++ WebApp5BySujit/Controllers/ProductcController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp5BySujit.Data;
+using WebApp5BySujit.Models;
+
+namespace WebApp5BySujit.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly ProductRepository _repo;
+    public ProductController(ProductRepository repo) => _repo = repo;
+
+    public IActionResult Index() => View(_repo.GetAll());
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public IActionResult Create(Product p) { _repo.Add(p); return RedirectToAction(nameof(Index)); }
+
+    public IActionResult Edit(int id) => View(_repo.GetById(id));
+
+    [HttpPost]
+    public IActionResult Edit(Product p) { _repo.Update(p); return RedirectToAction(nameof(Index)); }
+
+    public IActionResult Delete(int id) => View(_repo.GetById(id));
+
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id) { _repo.Delete(id); return RedirectToAction(nameof(Index)); }
+}
---
+++ WebApp5BySujit/Data/ProductRepository.cs
@@ -0,0 +1,82 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using WebApp5BySujit.Models;
+
+namespace WebApp5BySujit.Data;
+
+public class ProductRepository
+{
+    private readonly string _connectionString;
+    public ProductRepository(IConfiguration configuration) =>
+        _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+
+    private SqlConnection GetConnection() => new(_connectionString);
+
+    public List<Product> GetAll()
+    {
+        var list = new List<Product>();
+        using var conn = GetConnection();
+        using var cmd = new SqlCommand("SELECT * FROM Products", conn);
+        conn.Open();
+        using var reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            list.Add(new Product
+            {
+                Id = Convert.ToInt32(reader["Id"]),
+                Name = reader["Name"].ToString()!,
+                Price = Convert.ToDecimal(reader["Price"])
+            });
+        }
+        return list;
+    }
+
+    public Product? GetById(int id)
+    {
+        using var conn = GetConnection();
+        using var cmd = new SqlCommand("SELECT * FROM Products WHERE Id = @Id", conn);
+        cmd.Parameters.AddWithValue("@Id", id);
+        conn.Open();
+        using var reader = cmd.ExecuteReader();
+        if (reader.Read())
+        {
+            return new Product
+            {
+                Id = Convert.ToInt32(reader["Id"]),
+                Name = reader["Name"].ToString()!,
+                Price = Convert.ToDecimal(reader["Price"])
+            };
+        }
+        return null;
+    }
+
+    public void Add(Product p)
+    {
+        using var conn = GetConnection();
+        using var cmd = new SqlCommand("INSERT INTO Products (Name, Price) VALUES (@Name, @Price)", conn);
+        cmd.Parameters.AddWithValue("@Name", p.Name);
+        cmd.Parameters.AddWithValue("@Price", p.Price);
+        conn.Open();
+        cmd.ExecuteNonQuery();
+    }
+
+    public void Update(Product p)
+    {
+        using var conn = GetConnection();
+        using var cmd = new SqlCommand("UPDATE Products SET Name = @Name, Price = @Price WHERE Id = @Id", conn);
+        cmd.Parameters.AddWithValue("@Id", p.Id);
+        cmd.Parameters.AddWithValue("@Name", p.Name);
+        cmd.Parameters.AddWithValue("@Price", p.Price);
+        conn.Open();
+        cmd.ExecuteNonQuery();
+    }
+
+    public void Delete(int id)
+    {
+        using var conn = GetConnection();
+        using var cmd = new SqlCommand("DELETE FROM Products WHERE Id = @Id", conn);
+        cmd.Parameters.AddWithValue("@Id", id);
+        conn.Open();
+        cmd.ExecuteNonQuery();
+    }
+}
